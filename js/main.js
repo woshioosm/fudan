03b9987a0631f57344833f7585a3db43a4caac87
miao <shuimiao0915@163.com>
2016-03-21 00:50:58 +0800

    add

---
+++ js/main.js
@@ -0,0 +1,10 @@
+﻿function changeToLogin(){
+  	$("#registry").hide();
+	$("#login").show();
+	$("#myModalLabel").text("登录");
+}
+function changeToRegistry(){
+  	$("#login").hide();
+	$("#registry").show();
+	$("#myModalLabel").text("注册");
+}
